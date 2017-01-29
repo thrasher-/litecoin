612a70ea252c9cf43afeebd5a89f0ee927691c38
Adrian Gallagher <thrasher@addictionsoftware.com>
2017-01-29 07:00:00 -0800

    Litecoin: Protocol and default settings
    0) Adjust BIP30 enforcement values
    
    1) Reduce amount that peers can adjust our time to eliminate an attack vector. Thanks to
    coblee for this fix.
    
    2) Zeitgeist2 patch - thanks to Lolcust and ArtForz. This fixes an issue where a
    51% attack can change difficulty at will. Go back the full period unless it's the
    first retarget after genesis.
    
    3) Avoid overflow in CalculateNextWorkRequired(). Thanks to pooler for the overflow fix.
    
    4) Zeitgeist2 bool fshift bnNew.bits(). Thanks to romanornr for this path.
    
    5) SegWit ContextualCheckBlockHeader adjustment and extra coverage.
    
    6) Reject peer proto version below 70002. Thanks to wtogami for this patch.
    
    7) Send final alert message to nodes warning about removal of the alert system. Thanks to coblee for this patch.
    
    8) Adjust default settings for Litecoin.
    
    9) Adjust STALE_CHECK_INTERVAL value

--- test/functional/feature_nulldummy.py
+++ test/functional/feature_nulldummy.py
@@ -21,6 +21,7 @@ from test_framework.test_framework import BitcoinTestFramework
 from test_framework.util import assert_equal, assert_raises_rpc_error
 
 NULLDUMMY_ERROR = "non-mandatory-script-verify-flag (Dummy CHECKMULTISIG argument must be zero)"
+VB_TOP_BITS = 0x20000000
 
 def trueDummy(tx):
     scriptSig = CScript(tx.vin[0].scriptSig)
@@ -99,11 +100,10 @@ class NULLDUMMYTest(BitcoinTestFramework):
         self.log.info("Test 6: NULLDUMMY compliant base/witness transactions should be accepted to mempool and in block after activation [432]")
         for i in test6txs:
             self.nodes[0].sendrawtransaction(i.serialize_with_witness().hex(), 0)
-        self.block_submit(self.nodes[0], test6txs, True, True)
-
-    def block_submit(self, node, txs, witness=False, accept=False):
+        self.block_submit(self.nodes[0], test6txs, True, True, VB_TOP_BITS)
+    def block_submit(self, node, txs, witness=False, accept=False, version=4):
         block = create_block(self.tip, create_coinbase(self.lastblockheight + 1), self.lastblocktime + 1)
-        block.nVersion = 4
+        block.nVersion = version
         for tx in txs:
             tx.rehash()
             block.vtx.append(tx)
