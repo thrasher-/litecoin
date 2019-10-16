8b5a5c7902a6542a396831c687dc323f909939b7
Jeremy Rubin <j@rubin.io>
2019-10-16 14:37:18 -0700

    Add SignatureChecker method for checking StandardTemplateHash

--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -244,6 +244,11 @@ public:
          return false;
     }
 
+    virtual bool CheckStandardTemplateHash(const std::vector<unsigned char>& hash) const
+    {
+        return false;
+    }
+
     virtual ~BaseSignatureChecker() {}
 };
 
@@ -267,6 +272,7 @@ public:
     bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, const ScriptExecutionData& execdata, ScriptError* serror = nullptr) const override;
     bool CheckLockTime(const CScriptNum& nLockTime) const override;
     bool CheckSequence(const CScriptNum& nSequence) const override;
+    bool CheckStandardTemplateHash(const std::vector<unsigned char>& hash) const override;
 };
 
 using TransactionSignatureChecker = GenericTransactionSignatureChecker<CTransaction>;
